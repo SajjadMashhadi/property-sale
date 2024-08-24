6b8a66ab2feac1d1034280fa252dfb4f43124615
sajjadmashhadi <sajjadmashhadi7@gmail.com>
2024-08-24 10:03:02 +0330

    set up tailwind css for project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 
 function App() {
+  const [dark, setDark] = useState(false);
+
+  const handleClick = () => {
+    setDark(!dark);
+    document.body.classList.toggle("dark");
+  };
   return (
     <div>
-      <div>home</div>
+      <div className="text-blue-500 dark:text-red-500">home</div>
+      <button onClick={() => handleClick()}>toggle</button>
       <div>
         <Outlet />
       </div>
